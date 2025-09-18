3874c7caded9a0513fd53e0f3a1f60f5d73a4b6b
zacharyb12 <boualemzachary89@yahoo.fr>
2025-09-18 16:10:17 +0200

    output-demo 18-09

--- src/components/pages/exercices/exo-input-bindings/exo-input-bindings-enfant/exo-input-bindings-enfant.ts
+++ src/components/pages/exercices/exo-input-bindings/exo-input-bindings-enfant/exo-input-bindings-enfant.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Todo } from '../../../../../models/todo.model';
 
 @Component({
   selector: 'app-exo-input-bindings-enfant',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './exo-input-bindings-enfant.css'
 })
 export class ExoInputBindingsEnfant {
+
+@Input() listTodo : Todo[] = [];
+
+@Output() emmeteur : EventEmitter<number> = new EventEmitter<number>();
+
+@Output() emmeteurStatus : EventEmitter<number> = new EventEmitter<number>();
+
+
+removeTodoEnfant(id : number){
+this.emmeteur.emit(id);
+}
+
+changeStatus(id : number){
+this.emmeteurStatus.emit(id);
+}
 
 }
--- src/components/pages/exercices/exo-input-bindings/exo-input-bindings/exo-input-bindings.ts
+++ src/components/pages/exercices/exo-input-bindings/exo-input-bindings/exo-input-bindings.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import { User } from '../../../../../models/user.model';
+import { Todo } from '../../../../../models/todo.model';
+import { FormsModule } from '@angular/forms';
+import { ExoInputBindingsEnfant } from "../exo-input-bindings-enfant/exo-input-bindings-enfant";
 
 @Component({
   selector: 'app-exo-input-bindings',
-  imports: [],
+  imports: [
+    FormsModule,
+    ExoInputBindingsEnfant
+],
   templateUrl: './exo-input-bindings.html',
   styleUrl: './exo-input-bindings.css'
 })
 export class ExoInputBindings {
+
+  // Valeur transmise du parent vers l'enfant
+  listTodoParent : Todo[] = [];
+
+  // Variables qui permettent de creer un objet Todo pour l'ajouter a la liste
+  title : string = "";
+  description : string = "";
+  lastId : number = 0;
+
+  // la methode qui permet d'ajouter un Todo a la liste
+  addTodo(){
+  const newTodo : Todo = {
+
+    id : this.listTodoParent.length + 1,
+    title : this.title,
+    description : this.description,
+    status : false,
+  }
+
+  this.listTodoParent.push(newTodo);
+  
+  }
+
+  removeTodoParent(id : number){
+ this.listTodoParent =  this.listTodoParent.filter(todo => todo.id !== id) 
+  }
+
+  //[]
+  changeStatus(id : number){
+const todoIndex = this.listTodoParent.findIndex(element => element.id === id);
+
+this.listTodoParent[todoIndex].status = !this.listTodoParent[todoIndex].status;
+  }
+
 
 }
