db3e9e93bed449b85fae13ec5027d619ae03adda
zacharyb12 <boualemzachary89@yahoo.fr>
2025-09-19 15:20:35 +0200

    demo-service-2

--- src/components/pages/demo/demo-service/list-product-serv/list-product-serv.ts
+++ src/components/pages/demo/demo-service/list-product-serv/list-product-serv.ts
@@ -12,21 +12,33 @@ export class ListProductServ{
 
   ListProducts : Product[] = [];
 
+
+  // il faut avoir injecter le service pour pouvoir l'utiliser 
+  // pour appeler les methodes du service il faudra utiliser this.nomService.nomMethode()
+
   // // ancienne methode
   constructor(
     private readonly serviceDemoLocal: DemoService
   ) { 
- 
-  }
 
+    // on peut appeler le service directement dans le constructeur
+    // this.ListProducts = this.serviceDemoLocal.getProducts();
+ 
+
+    this.getProducts();
+  }
+  // nouvelle injection
+  private readonly serviceDemoNew = inject(DemoService)
+
+
+  // on peut creer une methode pour appeler le service
+  // et l'appeler dans le ngOnInit ou dans le constructeur
   getProducts(){
     this.ListProducts = this.serviceDemoLocal.getProducts();
   }
   
-  // // ngOnInit(): void {
-  // //   this.ListProducts = this.serviceDemoNew.getProducts();
-  // // }
+  ngOnInit(): void {
+    this.ListProducts = this.serviceDemoNew.getProducts();
+  }
 
-  // // nouvelle injection
-  // private readonly serviceDemoNew = inject(DemoService)
 }
