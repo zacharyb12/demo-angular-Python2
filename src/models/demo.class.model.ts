e9b16937b641b9cb920f2dbf1cf96250779ea322
zacharyb12 <boualemzachary89@yahoo.fr>
2025-10-08 11:03:45 +0200

    class generique

--- src/models/demo.class.model.ts
+++ src/models/demo.class.model.ts
@@ -12,3 +12,28 @@ export class DemoClassModel {
     return `${this.name}: ${this.description}`
   }
 }
+
+
+export class demoGenericClass<T> {
+    private items: T[] = []
+
+
+    addItem(item: T): void {
+        this.items.push(item)
+    }
+
+    getItems(): T[] {
+        return this.items
+    }
+
+    removeItem(item: T): void {
+        const index = this.items.indexOf(item)
+        if (index > -1) {
+            this.items.splice(index, 1)
+        }
+    }
+
+    getItemsByCondition(condition: (item: T) => boolean): T[] {
+        return this.items.filter(condition)
+    }
+}
